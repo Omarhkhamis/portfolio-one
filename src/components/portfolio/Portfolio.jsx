3fcf95644e01298de957055d2552f18be876da0f
Omar Alhaj <omar95net@gmail.com>
2023-08-02 11:08:09 +0400

    responsive

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,7 +13,7 @@ function Portfolio() {
   };
 
   return (
-    <section className="work container section" id="work">
+    <section className="work container section" id="portfolio">
       <h2 className="section__title">Recent Works</h2>
       <div className="work__filters">
         <span className="work__item" onClick={() => setItems(List)}>
